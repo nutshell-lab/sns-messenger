f7bbf70447c1b344109cb1b17d70649c4c68ef28
Victor Rebiard--Crépin <victor.rebiardcrepin@gmail.com>
2018-08-17 19:16:24 +0200

    Removed useless tests

--- test.js
+++
@@ -1,7 +0,0 @@
-import test from 'ava'
-import messenger from '.'
-
-test('throws resolve error', async t => {
-  const err = await t.throws(messenger('unkown-channel', 'yo', { foo: 'bar' }))
-  t.is(err.message, "Unable to resolve channel 'unkown-channel'")
-})
